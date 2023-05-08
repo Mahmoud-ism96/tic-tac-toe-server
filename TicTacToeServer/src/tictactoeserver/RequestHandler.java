54a810a9a8919a9c1d88d9f3ebf2de402e4202ff
mazen tarek <94558164+MazenTarek24@users.noreply.github.com>
2023-05-08 16:17:17 +0200

    data base

--- TicTacToeServer/src/tictactoeserver/RequestHandler.java
+++ TicTacToeServer/src/tictactoeserver/RequestHandler.java
@@ -14,6 +14,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Vector;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -23,7 +28,7 @@ import java.util.logging.Logger;
  * @author fg
  */
 public class RequestHandler  extends Thread {
-     private DataInputStream inputStream;
+    private DataInputStream inputStream;
     private PrintStream printStream;
     private BufferedReader bufferedReader;
     private String clientName;
@@ -39,24 +44,31 @@ public class RequestHandler  extends Thread {
     }
 
     public void run() {
-        try {
+       
             while (true) {
+                 try {
                 String jsonString = bufferedReader.readLine();
+                if(jsonString != null)
+                {
 
                 Gson gson = new Gson();
                 JsonElement jsonElement = gson.fromJson(jsonString, JsonElement.class);
                 JsonObject jsonObject = jsonElement.getAsJsonObject();
 
-                handleRequset(jsonObject);
-
-            }
-
-        } catch (IOException ex) {
+                    try {
+                        handleRequset(jsonObject);
+                    } catch (SQLException ex) {
+                        Logger.getLogger(RequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            } catch (IOException ex) {
             Logger.getLogger(RequestHandler.class.getName()).log(Level.SEVERE, null, ex);
+            
         }
     }
+    }
 
-    private void handleRequset(JsonObject jsonObject) {
+    private void handleRequset(JsonObject jsonObject) throws SQLException {
 
         JsonElement requestElement = jsonObject.get("request");
         JsonObject requestObject = requestElement.getAsJsonObject();
@@ -70,13 +82,42 @@ public class RequestHandler  extends Thread {
 
     }
 
-    private void signInRequest(JsonObject jsonObject) {
+    private void signInRequest(JsonObject jsonObject) throws SQLException {
         JsonElement signInElement = jsonObject.get("data");
         JsonObject signInObject = signInElement.getAsJsonObject();
         String userName = signInObject.get("username").getAsString();
         String password = signInObject.get("password").getAsString();
 
+         try {
+    Connection connection = DriverManager.
+            getConnection("jdbc:derby://localhost:1527/GameDataBase", "root", "root");
+
+        String query = "SELECT * FROM users WHERE username = ? AND password = ?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, userName);
+        statement.setString(2, password);
+
+        ResultSet resultSet = statement.executeQuery();
+
+        
+        if (resultSet.next()) {
+          
+            System.out.println("User exists in the database");
+        } else {
+          
+            System.out.println("User does not exist in the database");
+        }
+
+        // Clo
+        System.out.println("username " + userName);
+        System.out.println("password " + password);
+
+    }catch(Exception ex)
+    {
+    System.out.println(ex.getLocalizedMessage());
+
     }
+}
 }
 
 
